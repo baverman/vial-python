0796433c20932e6425caa5e38fcee9352a0c4a4c
Anton Bobrov <bobrov@vl.ru>
2013-06-02 10:03:13 +0400

    use vimfunction decorator

--- vial-plugin/python/plugin.py
+++ vial-plugin/python/plugin.py
@@ -1,24 +1,25 @@
 from vial import vfunc, vim
-from vial.utils import get_var
+from vial.utils import get_var, vimfunction
 
 from . import env
 
 def python_buffer(buf):
     vfunc.setbufvar(buf.number, '&omnifunc', 'VialPythonOmni')
 
-def omnifunc():
-    lvars = vim.bindeval('a:')
-    if lvars['findstart']:
-        lvars['result'] = vim.current.window.cursor[1] - 1
+@vimfunction
+def omnifunc(findstart, base):
+    if findstart:
+        return vim.current.window.cursor[1] - 1
     else:
-        lvars['result'] = ['boo', 'foo']
+        return ['boo', 'foo']
 
-def executable_choice():
+@vimfunction
+def executable_choice(start, cmdline, pos):
     executables = set(('default', 'python2', 'python3'))\
         .union(env.get_virtualenvwrapper_executables())\
         .union(get_var('vial_python_executables', {}))
 
-    vim.bindeval('a:')['result'] = '\n'.join(sorted(executables))
+    return '\n'.join(sorted(executables))
 
 def set_executable(name):
     vim.vars['vial_python_executable'] = name
