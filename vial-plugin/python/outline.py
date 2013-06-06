d22fb80168b18b2ceda2d1ebbb7dbbe0a0396bbc
Anton Bobrov <bobrov@vl.ru>
2013-06-06 20:48:26 +0400

    outline: show node path

--- vial-plugin/python/outline.py
+++ vial-plugin/python/outline.py
@@ -12,12 +12,48 @@ def show():
 
     dialog.open()
 
+
+class HoldIt(object):
+    def __init__(self, it):
+        self.it = it
+        self._hold = []
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self._hold:
+            return self._hold.pop(0)
+        else:
+            return next(self.it)
+
+    def hold(self, value):
+        self._hold.append(value)
+
+
 OUTLINE_REGEX = re.compile(r'(?m)^([ \t]*)(def|class)\s+(\w+)')
 def get_outline(source, tw):
-    for m in OUTLINE_REGEX.finditer(source):
-        ws = m.group(1)
-        level = len(ws.replace('\t', ' ' * tw)) // tw
-        yield level, m.group(2), m.group(3), m.start(2)
+    result = []
+    it = HoldIt(OUTLINE_REGEX.finditer(source))
+
+    def push_childs(plevel, parent):
+        for m in it:
+            ws = m.group(1)
+            level = len(ws.replace('\t', ' ' * tw)) // tw
+            if level == plevel:
+                result.append((level, m.group(2), parent + (m.group(3),), m.start(2)))
+            elif level > plevel:
+                it.hold(m)
+                push_childs(level, result[-1][2])
+            else:
+                it.hold(m)
+                return
+
+    push_childs(0, ())
+    return result
 
 class Outline(SearchDialog):
     def __init__(self):
@@ -28,7 +64,7 @@ class Outline(SearchDialog):
     def fill(self):
         self.list_view.clear()
         for l, t, name, start in self.outline:
-            self.items.append((name, '  ' * l + name, '', start))
+            self.items.append((name, '  ' * l + name[-1], '', start))
 
         self.list_view.render()
         self.loop.refresh()
@@ -45,7 +81,7 @@ class Outline(SearchDialog):
         self.last_window = vfunc.winnr()
 
         source = get_content()
-        self.outline = list(get_outline(source, vfunc.shiftwidth()))
+        self.outline = get_outline(source, vfunc.shiftwidth())
 
         self.show('')
         self.fill()
@@ -63,9 +99,11 @@ class Outline(SearchDialog):
         already_matched = {}
         for m in matchers:
             for l, t, name, start in self.outline:
-                if (start, name) not in already_matched and m(name):
-                    already_matched[(start, name)] = True
-                    self.items.append((name, name, '', start))
+                n = name[-1]
+                p = name[:-1]
+                if start not in already_matched and m(n):
+                    already_matched[start] = True
+                    self.items.append((name, n, ' / '.join(p), start))
 
         self.list_view.render()
         self.loop.refresh()
