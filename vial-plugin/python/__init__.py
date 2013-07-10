21c122bfae48a87f19ee68b62533d0f7e29af0ee
Anton Bobrov <bobrov@vl.ru>
2013-07-10 20:39:46 +0400

    <sid> for VialPythonSetExecutable completion func

--- vial-plugin/python/__init__.py
+++ vial-plugin/python/__init__.py
@@ -6,10 +6,10 @@ def init():
 
     vial.register_function('VialPythonOmni(findstart, base)', lfunc('.plugin.omnifunc'))
 
-    vial.register_function('VialPythonExecutableChoice(start, cmdline, pos)',
+    vial.register_function('<SID>VialPythonExecutableChoice(start, cmdline, pos)',
         lfunc('.plugin.executable_choice'))
     vial.register_command('VialPythonSetExecutable', lfunc('.plugin.set_executable'),
-        complete='custom,VialPythonExecutableChoice', nargs=1)
+        complete='custom,<SID>VialPythonExecutableChoice', nargs=1)
 
     vial.register_command('VialPythonGotoDefinition', lfunc('.plugin.goto_definition'))
     vial.register_command('VialPythonOutline', lfunc('.plugin.show_outline'))
